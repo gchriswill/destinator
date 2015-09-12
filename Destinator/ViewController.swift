9ad34b87520380fa1a4058ad239159d6f9221c55
Christopher Gonzalez <gchriswill@me.com>
2015-09-11 23:20:20 -0400

    Initial Commit

---
+++ Destinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Destinator
+//
+//  Created by Christopher Gonzalez on 9/11/15.
+//  Copyright (c) 2015 Christopher Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
